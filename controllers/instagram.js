5f2b8cf573fc67c657dd72fbef37f22534a441ae
Sergey Matyushkov <smatyushkov@arenamb.ru>
2020-09-01 16:00:40 +0300

    change insta lib

--- controllers/instagram.js
+++ controllers/instagram.js
@@ -3,8 +3,7 @@ const errorHandler = require("../utils/error");
 
 module.exports.getLastPosts = async function (req, res) {
   try {
-    // const posts = instapi.getPosts();
-    const posts = {message: 'test message'};
+    const posts = await instapi.getPosts();
     res.status(200).json(posts);
   } catch (e) {
     errorHandler(res, e);
--- utils/instapi.js
+++ utils/instapi.js
@@ -1,28 +1,36 @@
 /** Access to instagram account and get last 8 photos with links and captions */
-const Instagram = require("node-instagram").default;
+const instagramPosts = require("instagram-posts");
 
 require("dotenv").config();
+console.log(`process.env.INSTAGRAM_UNAME: ${process.env.INSTAGRAM_UNAME}`);
+const uname = process.env.INSTAGRAM_UNAME;
 
-console.log(`process.env.OSITE_INSTAGRAM_TOKEN: ${process.env.OSITE_INSTAGRAM_TOKEN}`);
-
-module.exports.getPosts = async function getPosts(quantity = 9) {
+module.exports.getPosts = async (quantity = 8) => {
   try {
-    const instagram = new Instagram({
-      accessToken: process.env.OSITE_INSTAGRAM_TOKEN,
-    });
-    // get last 8 photos
-    const data = await instagram.get("users/self/media/recent", {
-      count: quantity,
-    });
-
-    let images = data.data.map((item) => ({
+    const data = await instagramPosts(uname, { count: quantity });
+    /*
+      [
+          {
+              id: 'BRWBBbXjT40',
+              username: 'cats_of_instagram',
+              time: 1488904930,
+              type: 'image',
+              likes: 809,
+              comments: 10,
+              text: 'This is my post',
+              media: 'https://instagram.fbma1-1.fna.fbcdn.net/t51.2885-15/s640x640/sh0.08/e35/1231231_123123_1231231.jpg',
+              …
+          },
+          …
+      ]
+    */
+    const posts = data.map((item) => ({
       id: item.id,
-      link: item.link,
-      src: item.images.standard_resolution.url,
-      alt: item.caption.text,
+      link: item.url,
+      src: item.media,
+      alt: item.text,
     }));
-
-    return images;
+    return posts;
   } catch (err) {
     console.log("No instagram data. Error: ", err);
     return [];
